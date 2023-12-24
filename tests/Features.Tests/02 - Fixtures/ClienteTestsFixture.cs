0b13e9a93c94667f52e8dc5ddcce7fedfe293095
WilliamGBrito <will-dragoesdareal@hotmail.com>
2023-12-24 18:31:59 -0300

    Finalizado a implantação dos testes de unidade.

---
+++ tests/Features.Tests/02 - Fixtures/ClienteTestsFixture.cs
@@ -0,0 +1,44 @@
+﻿using Features.Clientes;
+using Xunit;
+
+namespace Features.Tests
+{
+    [CollectionDefinition(nameof(ClienteCollection))]
+    public class ClienteCollection : ICollectionFixture<ClienteTestsFixture>
+    { }
+
+    public class ClienteTestsFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "William",
+                "Brito",
+                DateTime.Now.AddYears(-30),
+                "will@teste.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        }
+
+        public Cliente GerarClienteInValido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "will2teste.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
